fbe30712c069f501da0ae6028abaf99f99e09920
jesse <jesse.p.tao@gmail.com>
2021-10-21 22:07:15 -0600

    start EDA on 3 indexer model

--- model/sim_setup.py
+++ model/sim_setup.py
@@ -2,7 +2,7 @@ print("running file: sim_setup.py")
 # T
 # SIMULATION_TIME_STEPS = 1576
 # SIMULATION_TIME_STEPS = 5000
-SIMULATION_TIME_STEPS = 90957
+SIMULATION_TIME_STEPS = 1867
 
 # N monte carlo runs
 MONTE_CARLO_RUNS = 1
